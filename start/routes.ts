af4fcb72f66ebd98dae6572a65e270fe79fe8732
Preslav Stoyanov <preslav.stoyanovbg@gmail.com>
2025-10-08 23:38:00 +0200

    PWA-37: Added login functionality

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,11 @@
 import router from '@adonisjs/core/services/router'
 
 const RegisterController = () => import('#controllers/auth/register_controller')
+const LoginController = () => import('#controllers/auth/login_controller')
 
 router
   .group(() => {
     router.post('register', [RegisterController, 'store'])
+    router.post('login', [LoginController, 'store'])
   })
   .prefix('auth')
