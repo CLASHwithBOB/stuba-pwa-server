d603cca2f080acd60c6354fef3b28a54eb04e097
Preslav Stoyanov <preslav.stoyanovbg@gmail.com>
2025-10-09 00:02:54 +0200

    PWA-39: Added me functionality

--- app/controllers/auth/logout_controller.ts
+++ app/controllers/auth/logout_controller.ts
@@ -2,7 +2,7 @@ import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LogoutController {
-  async store({ auth }: HttpContext) {
+  async destroy({ auth }: HttpContext) {
     const user = auth.user!
 
     await User.accessTokens.delete(user, user.currentAccessToken.identifier)
--- start/routes.ts
+++ start/routes.ts
@@ -1,11 +1,22 @@
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 const RegisterController = () => import('#controllers/auth/register_controller')
 const LoginController = () => import('#controllers/auth/login_controller')
+const LogoutController = () => import('#controllers/auth/logout_controller')
+const MeController = () => import('#controllers/auth/me_controller')
 
 router
   .group(() => {
-    router.post('register', [RegisterController, 'store'])
-    router.post('login', [LoginController, 'store'])
+    router.post('register', [RegisterController, 'store']).as('register')
+    router.post('login', [LoginController, 'store']).as('login')
+
+    router
+      .group(() => {
+        router.delete('logout', [LogoutController, 'destroy']).as('logout')
+        router.get('me', [MeController, 'index']).as('me')
+      })
+      .use(middleware.auth())
   })
   .prefix('auth')
+  .as('auth.')
