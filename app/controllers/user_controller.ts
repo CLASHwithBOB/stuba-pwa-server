99046889136d1d2b3a0d566cfce49c27a1c755b1
Preslav Stoyanov <preslav.stoyanovbg@gmail.com>
2025-10-10 20:24:36 +0200

    PWA-42: Added update user logic

---
+++ app/controllers/user_controller.ts
@@ -0,0 +1,15 @@
+import { updateValidator } from '#validators/user_validator'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class UserController {
+  async update({ request, response, auth }: HttpContext) {
+    const user = auth.user!
+    console.log(user)
+    const data = await request.validateUsing(updateValidator(user.id))
+
+    user.merge(data)
+    await user.save()
+
+    return response.ok(user)
+  }
+}
