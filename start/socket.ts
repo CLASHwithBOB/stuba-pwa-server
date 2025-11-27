ed4c0e29547b024827c7fafadcfab0f72224def5
Преслав Стоянов <85369896+Preslav-Stoyanov@users.noreply.github.com>
2025-11-27 02:33:37 +0100

    PWA-85: User is typing functionality (#21)

---
+++ start/socket.ts
@@ -0,0 +1,42 @@
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+
+const httpServer = createServer()
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('Client connected:', socket.id)
+
+  socket.on('join-channel', (channelId) => {
+    console.log(`Socket ${socket.id} joined channel ${channelId}`)
+    socket.join(`channel:${channelId}`)
+  })
+
+  socket.on('leave-channel', (channelId) => {
+    console.log(`Socket ${socket.id} left channel ${channelId}`)
+    socket.leave(`channel:${channelId}`)
+  })
+
+  socket.on('typing', ({ channelId, text, userId }) => {
+    io.to(`channel:${channelId}`).emit('typing', {
+      text,
+      userId,
+      channelId,
+    })
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id)
+  })
+})
+
+httpServer.listen(3334, () => {
+  console.log('Socket.IO server running on http://localhost:3334')
+})
+
+export default io
